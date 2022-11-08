4e1654217acffab585f89e524a99e4bdfd02466f
Your Name <you@example.com>
2022-11-08 16:14:49 +0700

    udpate

---
+++ bin/specta_apis_scheme.dart
@@ -0,0 +1,5 @@
+import 'package:specta_apis_scheme/specta_apis_scheme.dart' as specta_apis_scheme;
+
+void main(List<String> arguments) {
+  print('Hello world: ${specta_apis_scheme.calculate()}!');
+}
---
+++ test/specta_apis_scheme_test.dart
@@ -0,0 +1,8 @@
+import 'package:specta_apis_scheme/specta_apis_scheme.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
